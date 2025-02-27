d6fc08ee9ef7a6e7d4d092fd64cfa006a14a97e8
ma9o <magogagiovanni@gmail.com>
2025-02-27 10:43:29 +0100

    feat(data): add low cutoff to conversation_pair_candidates

--- apps/data-pipeline/data_pipeline/assets/ai_conversations/__init__.py
+++ apps/data-pipeline/data_pipeline/assets/ai_conversations/__init__.py
@@ -1,11 +1,12 @@
+from .conversation_pair_candidates import conversation_pair_candidates
 from .conversation_skeletons import conversation_skeletons
 from .conversation_summaries import conversation_summaries
 from .conversations_embeddings import conversations_embeddings
 from .long_range_causality import long_range_causality
 from .parsed_conversations import parsed_conversations
 from .serendipitous_paths import serendipitous_paths
-from .serendipity_simple import serendipity_simple
 from .serendipity_optimized import serendipity_optimized
+from .serendipity_simple import serendipity_simple
 from .short_range_causality import short_range_causality
 from .skeletons_embeddings import skeletons_embeddings
 
@@ -20,4 +21,5 @@ __all__ = [
     serendipitous_paths,
     serendipity_simple,
     serendipity_optimized,
+    conversation_pair_candidates,
 ]  # type: ignore
