bb33827ace482f2d202eaf803d6476c6b579d2a0
ma9o <magogagiovanni@gmail.com>
2025-03-04 21:07:33 +0100

    refactor(data): remove dead code

--- apps/data-pipeline/data_pipeline/assets/ai_conversations/__init__.py
+++ apps/data-pipeline/data_pipeline/assets/ai_conversations/__init__.py
@@ -1,29 +1,24 @@
 from .cluster_categorizations import cluster_categorizations
-from .conversation_pair_candidates import conversation_pair_candidates
 from .conversation_pair_clusters import conversation_pair_clusters
-from .conversation_skeletons import conversation_skeletons
 from .conversation_summaries import conversation_summaries
 from .conversations_embeddings import conversations_embeddings
-from .long_range_causality import long_range_causality
 from .parsed_conversations import parsed_conversations
-from .serendipitous_paths import serendipitous_paths
 from .serendipity_optimized import serendipity_optimized
-from .serendipity_simple import serendipity_simple
-from .short_range_causality import short_range_causality
-from .skeletons_embeddings import skeletons_embeddings
+
+# Deprecated assets
+# from .deprecated.conversation_pair_candidates import conversation_pair_candidates
+# from .deprecated.conversation_skeletons import conversation_skeletons
+# from .deprecated.long_range_causality import long_range_causality
+# from .deprecated.serendipitous_paths import serendipitous_paths
+# from .deprecated.serendipity_simple import serendipity_simple
+# from .deprecated.short_range_causality import short_range_causality
+# from .deprecated.skeletons_embeddings import skeletons_embeddings
 
 __all__ = [
     parsed_conversations,
-    conversation_skeletons,
     conversation_summaries,
     conversations_embeddings,
-    skeletons_embeddings,
-    short_range_causality,
-    long_range_causality,
-    serendipitous_paths,
-    serendipity_simple,
     serendipity_optimized,
-    conversation_pair_candidates,
     conversation_pair_clusters,
     cluster_categorizations,
 ]  # type: ignore
