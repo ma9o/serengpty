6b1525d7c3454e76aeee54fab2f0d7a1ff8bde86
ma9o <magogagiovanni@gmail.com>
2025-03-20 11:06:41 +0100

    feat(serengpty-extension-api): enhance database configuration and update schema with new indexing

--- apps/serengpty-extension-api/drizzle.config.ts
+++ apps/serengpty-extension-api/drizzle.config.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const dbUrl = new URL(process.env.DATABASE_URL!);
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/app/services/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    ssl: true,
+    host: dbUrl.hostname,
+    port: parseInt(dbUrl.port),
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.slice(1),
   },
 });
--- apps/serengpty-extension-api/src/app/services/db/schema.ts
+++ apps/serengpty-extension-api/src/app/services/db/schema.ts
@@ -5,8 +5,9 @@ import {
   timestamp,
   text,
   varchar,
-  vector,
   uuid,
+  index,
+  halfvec,
 } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
@@ -24,18 +25,27 @@ export const usersTable = pgTable('users', {
   updatedAt: timestamp().notNull().defaultNow(),
 });
 
-export const conversationsTable = pgTable('conversations', {
-  id: uuid('id').primaryKey().defaultRandom(),
+export const conversationsTable = pgTable(
+  'conversations',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
 
-  title: varchar({ length: 255 }).notNull(),
-  content: text().notNull(),
-  embedding: vector({ dimensions: 3072 }).notNull(),
+    title: varchar({ length: 255 }).notNull(),
+    content: text().notNull(),
+    embedding: halfvec({ dimensions: 3072 }).notNull(),
 
-  userId: integer().references(() => usersTable.id),
+    userId: integer().references(() => usersTable.id),
 
-  createdAt: timestamp().notNull().defaultNow(),
-  updatedAt: timestamp().notNull().defaultNow(),
-});
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp().notNull().defaultNow(),
+  },
+  (table) => [
+    index('embeddingIndex').using(
+      'hnsw',
+      table.embedding.op('halfvec_cosine_ops')
+    ),
+  ]
+);
 
 export const usersRelations = relations(usersTable, ({ many }) => ({
   conversations: many(conversationsTable),
