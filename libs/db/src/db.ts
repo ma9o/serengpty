b1fec2a6e028237e0d9fcd612cc24c4e2f80115f
ma9o <magogagiovanni@gmail.com>
2025-03-28 11:00:10 +0100

    refactor(db): replace inline connection configuration with pg Pool instance for improved database connection management

--- libs/db/src/db.ts
+++ libs/db/src/db.ts
@@ -1,10 +1,13 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env['DATABASE_URL'],
+  ssl: process.env['NODE_ENV'] === 'production',
+});
 
 export const db = drizzle({
-  connection: {
-    connectionString: process.env['DATABASE_URL'],
-    ssl: process.env['NODE_ENV'] === 'production',
-  },
+  client: pool,
   schema: schema,
 });
